74c73e011e9e9cf255f5aaa503c5a79b0e54f5dd
AvraamNikolaou <avnikolaou@hotmail.com>
2019-09-24 12:31:32 +0300

    fix: basic boiler and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import './Projects/Home/App.css';
+import  Home  from "./Projects/Home/Home"
+import Looplab from "./Projects/LoopLab_Social_Theme/Looplab";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Route exact path = "/" render = {() => (
+              <Home/>
+          )}
+          />
+
+          <Route exact path = "/Looplab" render = {() => (
+              <Looplab/>
+          )}
+          />
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Projects/LoopLab_Social_Theme/Looplab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Looplab() {
+  return (
+      <div className="App">
+        <p>This is the LoppLab Theme</p>
+      </div>
+  );
+}
+
+export default Looplab;
