f6e748602af1d608b365eec824438aafe1f95916
AvraamNikolaou <avnikolaou@hotmail.com>
2019-09-26 19:02:17 +0300

    fix: project 3 about page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './Projects/Home/App.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab, faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCheck, faArrowRight, faEnvelopeOpen, faArrowCircleDown, faUser, faEnvelope, faPencilAlt,
-    faCog, faCloud, faCartPlus, faPlay } from '@fortawesome/free-solid-svg-icons'
+    faCog, faCloud, faCartPlus, faPlay, faBuilding, faBullhorn, faComments, faBox, faCreditCard, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import  Home  from "./Projects/Home/Home"
 import Looplab from "./Projects/LoopLab_Social_Theme/Looplab";
 import Mizuxe from "./Projects/Mizuxe_Book_Theme/Mizuxe";
 import Glozzom from "./Projects/Glozzom_Multi_Page_Theme/Glozzom";
 
 library.add(fab, faCheckSquare, faCheck, faArrowRight, faEnvelopeOpen, faArrowCircleDown,
-    faFacebook, faInstagram, faTwitter, faUser, faEnvelope, faPencilAlt, faCog, faCloud, faCartPlus, faPlay);
+    faFacebook, faInstagram, faTwitter, faUser, faEnvelope, faPencilAlt, faCog, faCloud, faCartPlus, faPlay,
+    faBuilding, faBullhorn, faComments, faBox, faCreditCard, faCoffee);
 
 class App extends Component {
 
--- src/Projects/Glozzom_Multi_Page_Theme/About.js
+++ src/Projects/Glozzom_Multi_Page_Theme/About.js
@@ -1,11 +1,171 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Slider from "react-slick";
+import './App.scss'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class About extends Component {
 
+    componentDidMount() {
+
+    }
+
     render() {
+
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+
         return (
             <div>
-                ABOUT PAGE!
+                {/*PAGE HEADER*/}
+                <header id={"page-header"}>
+                    <div className={"container"}>
+                        <div className={"row"}>
+                            <div className={"col-md-6 m-auto text-center"}>
+                                <h1>About Us</h1>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas, temporibus?</p>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                {/*ABOUT SECTION*/}
+                <section id={"about"} className={"py-3"}>
+                    <div className={"container"}>
+                        <div className={"row"}>
+                            <div className={"col-md-6"}>
+                                <h1>What We Do</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                                    error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                                    laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                                    praesentium temporibus adipisci debitis labore!</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, fuga eaque. Amet, assumenda aliquid tempore dolorum
+                                    error eveniet doloribus sed repellat quod, dolores fuga ipsam soluta. Aliquam accusantium pariatur aut sint deleniti
+                                    laborum ducimus voluptatem? Architecto cumque quod suscipit officiis soluta, voluptate dicta blanditiis similique
+                                    praesentium temporibus adipisci debitis labore!</p>
+                            </div>
+                            <div className={"col-md-6"}>
+                                <img src="https://source.unsplash.com/random/700x700/?technology" alt="" className={"img-fluid rounded-circle d-none d-md-block about-img"}/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/*ICON BOXES*/}
+                <section id="icon-boxes" className={"p-5"}>
+                    <div className={"container"}>
+                        <div className={"row mb-4"}>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-danger text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"building"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-dark text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"bullhorn"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-danger text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"comments"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={"row mb-4"}>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-dark text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"box"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-danger text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"credit-card"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={"col-md-4"}>
+                                <div className={"card bg-dark text-white text-center"}>
+                                    <div className={"card-body"}>
+                                        <FontAwesomeIcon icon={"coffee"} className={"fa-3x"}/>
+                                        <h3>Sample Heading</h3>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, adipisci.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/*TESTIMONIASL*/}
+                <section id="testimonials" className={"p-4 bg-dark text-white"}>
+                    <div className={"container"}>
+                        <h2 className={"text-center"}>testimonials</h2>
+                        <div className={"row text-center"}>
+                            <div className={"col"}>
+                                <div className={"slider"}>
+                                    <Slider {...settings}>
+                                        <div>
+                                            <blockquote className={"blockquote"}>
+                                                <p className={"mb-0"}>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quaerat.
+                                                </p>
+                                                <footer className={"blockquote-footer"}>John Doe From
+                                                    <cite title={"Company 1"}> Company 1</cite>
+                                                </footer>
+                                            </blockquote>
+                                        </div>
+                                        <div>
+                                            <blockquote className={"blockquote"}>
+                                                <p className={"mb-0"}>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quaerat.
+                                                </p>
+                                                <footer className={"blockquote-footer"}>Sam Smith From
+                                                    <cite title={"Company 2"}> Company 2</cite>
+                                                </footer>
+                                            </blockquote>
+                                        </div>
+                                        <div>
+                                            <blockquote className={"blockquote"}>
+                                                <p className={"mb-0"}>
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quaerat.
+                                                </p>
+                                                <footer className={"blockquote-footer"}>Meghan Williams From
+                                                    <cite title={"Company 3"}> Company 3</cite>
+                                                </footer>
+                                            </blockquote>
+                                        </div>
+                                    </Slider>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         );
     }
