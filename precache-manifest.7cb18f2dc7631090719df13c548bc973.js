d8fd72e5cebf5bc1272d5c5b38b335f22b475fbd
AvraamNikolaou <avnikolaou@hotmail.com>
2019-10-02 14:38:22 +0300

    Updates

---
+++ precache-manifest.7cb18f2dc7631090719df13c548bc973.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add97a7ff5b8dbf56939b1a231cc036e",
+    "url": "/bootstrap-4-themes/index.html"
+  },
+  {
+    "revision": "6229123a6cff520da387",
+    "url": "/bootstrap-4-themes/static/css/2.6169fff2.chunk.css"
+  },
+  {
+    "revision": "55df4d0aaa9a46e680d1",
+    "url": "/bootstrap-4-themes/static/css/main.1284f102.chunk.css"
+  },
+  {
+    "revision": "6229123a6cff520da387",
+    "url": "/bootstrap-4-themes/static/js/2.867575a2.chunk.js"
+  },
+  {
+    "revision": "55df4d0aaa9a46e680d1",
+    "url": "/bootstrap-4-themes/static/js/main.02fbe36f.chunk.js"
+  },
+  {
+    "revision": "9ad098b6aa3d2f835295",
+    "url": "/bootstrap-4-themes/static/js/runtime~main.556565e2.js"
+  },
+  {
+    "revision": "5f92255b37e341a0638e2243bd205832",
+    "url": "/bootstrap-4-themes/static/media/avatar.5f92255b.png"
+  },
+  {
+    "revision": "d77715c3679cb995f9a63e5c7dca0328",
+    "url": "/bootstrap-4-themes/static/media/book.d77715c3.png"
+  },
+  {
+    "revision": "a58d65581e78930a9aa96017155c7968",
+    "url": "/bootstrap-4-themes/static/media/create-section1.a58d6558.jpg"
+  },
+  {
+    "revision": "38bb4d9aff8bf3d2c6d6b71744967188",
+    "url": "/bootstrap-4-themes/static/media/explore-section1.38bb4d9a.jpg"
+  },
+  {
+    "revision": "e2dc0ca7b7e33c30f812caadff6d6aef",
+    "url": "/bootstrap-4-themes/static/media/home.e2dc0ca7.jpg"
+  },
+  {
+    "revision": "66abf4bbae68866efa2ccce88679e329",
+    "url": "/bootstrap-4-themes/static/media/image1.66abf4bb.jpg"
+  },
+  {
+    "revision": "e2f271ec4a61640b23a5cae098c17e3a",
+    "url": "/bootstrap-4-themes/static/media/image2.e2f271ec.jpg"
+  },
+  {
+    "revision": "60c25ecf4e37ddd93f45799dbec05ddb",
+    "url": "/bootstrap-4-themes/static/media/image3.60c25ecf.jpg"
+  },
+  {
+    "revision": "63891ba6bff4b88b1cdd6319c6d22d09",
+    "url": "/bootstrap-4-themes/static/media/laptop.63891ba6.png"
+  },
+  {
+    "revision": "7a9f214177f6fab746587f60620d8054",
+    "url": "/bootstrap-4-themes/static/media/lights.7a9f2141.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bootstrap-4-themes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "78d7b0dfa3e3d7c956eea6cfbec668ae",
+    "url": "/bootstrap-4-themes/static/media/media.78d7b0df.jpg"
+  },
+  {
+    "revision": "78e58366f4c09e92cbe7ab33f9bfaf34",
+    "url": "/bootstrap-4-themes/static/media/mlogo.78e58366.png"
+  },
+  {
+    "revision": "e273a35547d03c3d0811e55ba0585195",
+    "url": "/bootstrap-4-themes/static/media/mountains.e273a355.jpg"
+  },
+  {
+    "revision": "00f0cc1faa59c2304634d16e2bbe961f",
+    "url": "/bootstrap-4-themes/static/media/person1.00f0cc1f.jpg"
+  },
+  {
+    "revision": "92fe23489f58e852af9a19ba8de8d83f",
+    "url": "/bootstrap-4-themes/static/media/person1.92fe2348.jpg"
+  },
+  {
+    "revision": "6832134561bbcfff304c7538ae9253c1",
+    "url": "/bootstrap-4-themes/static/media/person2.68321345.jpg"
+  },
+  {
+    "revision": "63cae7329b20cc126ed68aab7379ae24",
+    "url": "/bootstrap-4-themes/static/media/person3.63cae732.jpg"
+  },
+  {
+    "revision": "e11da43372c4951d7a22657933b18462",
+    "url": "/bootstrap-4-themes/static/media/person4.e11da433.jpg"
+  },
+  {
+    "revision": "c59ce5249f0247cf33fff15fdffea569",
+    "url": "/bootstrap-4-themes/static/media/share-section1.c59ce524.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/bootstrap-4-themes/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/bootstrap-4-themes/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/bootstrap-4-themes/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/bootstrap-4-themes/static/media/slick.f97e3bbf.svg"
+  }
+]);
